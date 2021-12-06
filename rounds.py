9ea0b12d594f761d4c5eb2c799e5bf4e8627d5d7
LauraJohnston20 <95102375+LauraJohnston20@users.noreply.github.com>
2021-12-06 14:05:02 -0330

    Update rounds.py
    updated main to dealer

--- rounds.py
+++ rounds.py
@@ -7,15 +7,15 @@ def round(deck, players):
     time.sleep(0.5)
     print("\n----------------------------LETS PLAY!------------------------------")
     for index, player in enumerate(players):
-        if main.card_total(player["hand"]) == 21:
+        if dealer.card_total(player["hand"]) == 21:
             print("---------------------------------------------------------------------")
             print("\nPLAYER " + str(index+1) + " HAS BLACKJACK")
             print("---------------------------------------------------------------------")
 
-        elif main.card_total(player["hand"]) < 21:
+        elif dealer.card_total(player["hand"]) < 21:
             if player["lose"] != True:  
                 time.sleep(0.5)
-                score = main.card_total(player["hand"])
+                score = dealer.card_total(player["hand"])
                 print("\n--------------------------------------------------------------------")
                 print("\nPlayer " + str(index+1) + ": You have " + str(score))
                 print(player)
@@ -29,7 +29,7 @@ def play(player, deck):
                 card = deck.pop()
                 print(str(card[0]) + " of " + str(card[1]))
                 player["hand"].append(card)
-                score = main.card_total(player["hand"])
+                score = dealer.card_total(player["hand"])
                 print("Total score: " + str(score))
                 if score > 21:
                     print("BUST. You lose your bet.")
@@ -38,7 +38,7 @@ def play(player, deck):
                     print("BLACKJACK")
                     break
             elif deal.lower()=="stay" or deal.lower() == "s":
-                score = main.card_total(player["hand"])
+                score = dealer.card_total(player["hand"])
                 print("Stay at " + str(score))
                 break
             else:
