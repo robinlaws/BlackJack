48dc1edfcbd89282f3469ac62e9e5a176d032a3c
robinlaws <94911535+robinlaws@users.noreply.github.com>
2021-12-08 08:29:52 -0330

    fixed comment spacing
    fixed comment spacing

--- dealer.py
+++ dealer.py
@@ -21,7 +21,7 @@ def card_total(hand):
     return total
 
 def main():
-##Print rules, get players, get balance.
+# Print rules, get players, get balance.
     menu.menu()
     while True:
         players = menu.get_players()
@@ -31,7 +31,7 @@ def main():
             menu.reset_player_hands(players)
             menu.get_bet(players)
 
-    ##Fill deck, shuffle deck, deal cards and get player scores
+# Fill deck, shuffle deck, deal cards and get player scores
             deck = []
             dealer_hand = []
             deal.fill_deck(deck)
@@ -44,16 +44,16 @@ def main():
             deal.deal_dealer_cards(dealer_hand, deck, 2)
             deal.get_player_scores(players)
             
-    ##Go into round for player (hit or stay) then proceed to dealer round
+# Player round (hit or stay) then proceed to dealer round
             rounds.round(deck, players)
             deal.get_dealer_score(dealer_hand)
             rounds.dealer_round(dealer_hand, deck)
 
-    ##Compare scores and get winners
+# Compare scores and get winners
             winner.get_final_points(players, dealer_hand)
             winner.get_winner(players, dealer_hand)
 
-    ##Options for progressing with game           
+# Options for progressing with game           
             time.sleep(0.5)
             choice = input("\nMove to the next round? (y/n): ")
             if choice.lower() == "n":
